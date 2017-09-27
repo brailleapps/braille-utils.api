7b7fc78cdd8616902be6a18ba06afe73a5ec64ce
Joel Håkansson <joel.hakansson@mtm.se>
2017-09-27 14:10:02 +0200

    Deprecates unnecessary methods on EmbosserWriterProperties
    Note that in order to deprecate methods from this interface, but not from EmbosserProperties (the super interface), methods have been copied from the super interface and deprecated.

--- src/org/daisy/braille/utils/api/embosser/EmbosserWriterProperties.java
+++ src/org/daisy/braille/utils/api/embosser/EmbosserWriterProperties.java
@@ -32,7 +32,79 @@ public interface EmbosserWriterProperties extends EmbosserProperties {
 	/**
 	 * Gets the maximum page height in the current configuration
 	 * @return returns the maximum page height, in rows
+	 * @deprecated Deprecated without replacement
 	 */
-	public int getMaxHeight();
+	@Deprecated
+	public default int getMaxHeight() {
+		throw new UnsupportedOperationException();
+	}
 
+	/**
+	 * Returns true if this embosser supports aligning. This indicates
+	 * that rows can be padded with whitespace to move the text block
+	 * horizontally using the value returned by <code>getMaxWidth</code>.
+	 * Should return true for all physical embossers, since they all have
+	 * a finite row length.
+	 * @return returns true if this embosser supports aligning, false otherwise.
+	 */
+	public boolean supportsAligning();
+
+	/**
+	 * Returns true if this embosser has some method for volume handling
+	 * @return returns true if this embosser supports volumes
+	 * @deprecated Deprecated without replacement
+	 */
+	@Deprecated
+	//TODO: When removing these methods, don't forget to remove the inheritance from EmbosserProperties
+	public default boolean supportsVolumes() {
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Returns true if this embosser supports 8 dot braille
+	 * @return returns true if this embosser supports 8 dot braille
+	 * @deprecated Deprecated without replacement
+	 */
+	@Deprecated
+	public default boolean supports8dot(){
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Returns true if this embosser supports duplex printing
+	 * @return returns true if this embosser supports duplex printing
+	 * @deprecated Deprecated without replacement
+	 */
+	@Deprecated
+	public default boolean supportsDuplex() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Returns true if this embosser supports z-folding. This indicates
+	 * that, if tractor paper is used, the embosser can emboss every
+	 * other paper upside down with the rear side up so that pages are
+	 * ordered face up as they fold naturally in the output stack.
+	 * @return returns true if this embosser supports z-folding, false otherwise.
+	 * @deprecated Deprecated without replacement
+	 */
+	@Deprecated
+	public default boolean supportsZFolding() {
+		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Returns true if this embosser supports magazine layout. This indicates
+	 * that the embosser can reorder pages and emboss two pages side-by-side
+	 * on the same side of the paper (and two more on the other side), so that
+	 * a readable document is created by stapling and folding the output stack
+	 * in the middle.
+	 * @param mode the print mode
+	 * @return returns true if this embosser supports magazine layout, false otherwise.
+	 * @deprecated Deprecated without replacement
+	 */
+	@Deprecated
+	public default boolean supportsPrintMode(PrintMode mode) {
+		throw new UnsupportedOperationException();
+	}
 }
