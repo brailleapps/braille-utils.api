6ee449bdb8349f5473ff4edd192c2273a37373c7
Joel Håkansson <joel.hakansson@mtm.se>
2018-09-05 14:51:20 +0200

    Keeps public API of CustomPaperCollection, but uses new implementation

--- src/org/daisy/braille/utils/api/paper/CustomPaperCollection.java
+++ src/org/daisy/braille/utils/api/paper/CustomPaperCollection.java
@@ -17,16 +17,8 @@
  */
 package org.daisy.braille.utils.api.paper;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 
 /**
  * Provides a custom paper collection that lets a user
@@ -35,27 +27,8 @@ import java.util.Date;
  */
 public class CustomPaperCollection {
 	private static CustomPaperCollection collection;
-	private final File f;
-	private ArrayList<Paper> papers;
-	private Integer index;
-	private Date sync;
 
 	private CustomPaperCollection() {
-		this.papers = new ArrayList<>();
-		this.index = 0;
-		this.sync = null;
-		File tmp = null;
-		try {
-			tmp = new File(System.getProperty("user.home"), CustomPaperCollection.class.getCanonicalName() + ".obj");
-		} catch (Exception e) {
-			// silently fail here
-		}
-		this.f = tmp;
-		try {
-			syncWithFile();
-		} catch (IOException e) {
-			// silently fail here
-		}
 	}
 
 	/**
@@ -74,7 +47,7 @@ public class CustomPaperCollection {
 	 * @return returns a collection of papers
 	 */
 	public synchronized Collection<Paper> list() {
-		return papers;
+		return UserPapersCollection.getInstance().list();
 	}
 
 	/**
@@ -87,7 +60,7 @@ public class CustomPaperCollection {
 	 * @throws IOException if an I/O error occurs
 	 */
 	public synchronized SheetPaper addNewSheetPaper(String name, String desc, Length width, Length height) throws IOException {
-		return (SheetPaper)add(new SheetPaper(name, desc, nextIdentifier(), width, height));
+		return UserPapersCollection.getInstance().addNewSheetPaper(name, desc, width, height);
 	}
 
 	/**
@@ -100,7 +73,7 @@ public class CustomPaperCollection {
 	 * @throws IOException if an I/O error occurs
 	 */
 	public synchronized TractorPaper addNewTractorPaper(String name, String desc, Length across, Length along) throws IOException {
-		return (TractorPaper)add(new TractorPaper(name, desc, nextIdentifier(), across,	along));
+		return UserPapersCollection.getInstance().addNewTractorPaper(name, desc, across, along);
 	}
 
 	/**
@@ -112,14 +85,7 @@ public class CustomPaperCollection {
 	 * @throws IOException if an I/O error occurs
 	 */
 	public synchronized RollPaper addNewRollPaper(String name, String desc, Length across) throws IOException  {
-		return (RollPaper)add(new RollPaper(name, desc, nextIdentifier(), across));
-	}
-
-	private Paper add(Paper p) throws IOException {
-		syncWithFile();
-		papers.add(p);
-		updateFile();
-		return p;
+		return UserPapersCollection.getInstance().addNewRollPaper(name, desc, across);
 	}
 
 	/**
@@ -128,73 +94,7 @@ public class CustomPaperCollection {
 	 * @throws IOException if an I/O error occurs
 	 */
 	public synchronized void remove(Paper p) throws IOException {
-		syncWithFile();
-		papers.remove(p);
-		updateFile();
+		UserPapersCollection.getInstance().remove(p);
 	}
 
-	private String nextIdentifier() {
-		index++;
-		return CustomPaperCollection.class.getCanonicalName()+"_"+index;
-	}
-
-	@SuppressWarnings("unchecked")
-	private void syncWithFile() throws IOException {
-		if (f==null) {
-			throw new FileNotFoundException();
-		}
-		if (sync == null || new Date(f.lastModified()).after(sync)) {
-			if (f.exists()) {
-				ObjectInputStream ois = null;
-				//FileLock lock = null;
-				try {
-					FileInputStream is = new FileInputStream(f);
-					//lock = is.getChannel().tryLock();
-					ois = new ObjectInputStream(is);
-					index = (Integer)ois.readObject();
-					papers = (ArrayList<Paper>)ois.readObject();
-					sync = new Date(f.lastModified());
-				} catch (IOException e) {
-					throw e;
-				} catch (Exception e) {
-					e.printStackTrace();
-					if (!f.delete()) {
-						f.deleteOnExit();
-					}
-					sync = null;
-				} finally {
-					if (ois!=null) {
-						try {
-							ois.close();
-						} catch (IOException e) { }
-					}
-				}
-			}
-		}
-	}
-
-	private void updateFile() throws IOException {
-		if (f==null) {
-			throw new FileNotFoundException();
-		}
-		ObjectOutputStream oos = null;
-		FileOutputStream os;
-		try {
-			os = new FileOutputStream(f);
-			oos = new ObjectOutputStream(os);
-			oos.writeObject(index);
-			oos.writeObject(papers);
-		} catch (IOException e) {
-			throw e;
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (oos != null) {
-				try {
-					oos.close();
-				} catch (IOException e) { }
-			}
-			sync = new Date(f.lastModified());
-		}
-	}
 }
